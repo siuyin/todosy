35411faa159e35b7c1f62a2582c499a22b4448a6
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-12-24 11:19:59 +0800

    Initial commit.

---
+++ lib/screens/tasks.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:todosy/constants.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: kPrimaryAppColor,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 35,
+        ),
+        onPressed: () {
+          print('pushed Floating Action Button');
+        },
+      ),
+      backgroundColor: kPrimaryAppColor,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 10,
+                left: 10,
+                bottom: 10,
+                right: 10,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.list,
+                      size: 40,
+                      color: kPrimaryAppColor,
+                    ),
+                    backgroundColor: Colors.white,
+                    radius: 30,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'TodoSY',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    '12 tasks',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: ListView(
+                  padding: EdgeInsets.only(
+                    top: 10,
+                    left: 10,
+                    right: 10,
+                  ),
+                  children: <Widget>[
+                    TodoItem(text: 'one'),
+                    TodoItem(text: 'two'),
+                  ],
+                ),
+                margin: EdgeInsets.only(
+                  top: 10,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TodoItem extends StatefulWidget {
+  final String text;
+
+  TodoItem({this.text});
+  @override
+  _TodoItemState createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  String text;
+  bool done = false;
+  @override
+  void initState() {
+    text = widget.text;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8.0,
+      ),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            done = !done;
+          });
+          print('tapped $text');
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 18,
+                color: done ? Colors.grey : Colors.black,
+              ),
+            ),
+            Icon(done ? Icons.check_box : Icons.check_box_outline_blank),
+          ],
+        ),
+      ),
+    );
+  }
+}
